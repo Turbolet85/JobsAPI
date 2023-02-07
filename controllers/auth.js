ece4d6ad95da49ed909c291f8921769b2d097fe9
Anton Soldatov <turbolet85@gmail.com>
2023-02-07 21:56:53 +0100

    add user, hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,11 @@
 const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+const bcrypt = require('bcryptjs');
 
-const register = (req, res) => {
-   res.send('register user');
+const register = async (req, res) => {
+   const user = await User.create({ ...req.body });
+   res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = (req, res) => {
