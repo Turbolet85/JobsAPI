78d2bb8ec019e7a73533538b5a090015a01e7a5a
Anton Soldatov <turbolet85@gmail.com>
2023-02-12 13:07:09 +0100

    add swagger docs, final commit

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
@@ -29,6 +33,12 @@ app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+//root
+app.get('/', (req, res) => {
+   res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
