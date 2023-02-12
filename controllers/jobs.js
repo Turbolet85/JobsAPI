3a64bf460d39530abc0ab35c08c85b18a7d51c80
Anton Soldatov <turbolet85@gmail.com>
2023-02-12 10:46:03 +0100

    pre deploy

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -33,11 +33,40 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-   res.send('update job');
+   const {
+      body: { company, position },
+      user: { userId },
+      params: { id: jobId },
+   } = req;
+   if (company === '' || position === '') {
+      throw new BadRequestError('Company or Position fields can not be empty');
+   }
+
+   const job = await Job.findByIdAndUpdate(
+      { _id: jobId, createdBy: userId },
+      req.body,
+      { new: true, runValidators: true }
+   );
+   if (!job) {
+      throw new NotFoundError(`No job with id ${jobId}`);
+   }
+   res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-   res.send('delete job');
+   const {
+      user: { userId },
+      params: { id: jobId },
+   } = req;
+
+   const job = await Job.findByIdAndRemove({
+      _id: jobId,
+      createdBy: userId,
+   });
+   if (!job) {
+      throw new NotFoundError(`No job with id ${jobId}`);
+   }
+   res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
